e2a57e2df5860fd6e4a83faa3266ee7c965bc475
zzolikoff1027 <78103329+zzolikoff1027@users.noreply.github.com>
2023-04-06 23:21:05 -0700

    Adding Assets

---
+++ Assets/Scripts/PatrolCoroutines.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class PatrolCoroutines : MonoBehaviour
+{
+
+    public Transform[] waypoints;
+    private int _currentWaypointIndex = 0;
+    private float _speed = 2f;
+
+    private float _waitTime = 1f; // in seconds
+
+    private Coroutine _prevCoroutine;
+
+    private void Start()
+    {
+        _prevCoroutine = StartCoroutine(_MovingToNextWaypoint());
+    }
+
+    private IEnumerator _MovingToNextWaypoint()
+    {
+        Transform wp = waypoints[_currentWaypointIndex];
+
+        while (Vector3.Distance(transform.position, wp.position) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, wp.position, _speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = wp.position;
+        yield return new WaitForSeconds(_waitTime);
+        _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+
+        StopCoroutine(_prevCoroutine);
+        _prevCoroutine = StartCoroutine(_MovingToNextWaypoint());
+    }
+
+}
